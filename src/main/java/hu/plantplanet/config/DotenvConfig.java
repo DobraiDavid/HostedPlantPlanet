b38a711736fbaf68c8bd885842dfcef70ac8862a
Dobrai David <dobraid21@gmail.com>
2025-02-11 20:52:06 +0100

    Implemented .env file

--- src/main/java/hu/plantplanet/config/DotenvConfig.java
+++ src/main/java/hu/plantplanet/config/DotenvConfig.java
@@ -1,4 +1,19 @@
 package hu.plantplanet.config;
 
-public class fd {
+import io.github.cdimascio.dotenv.Dotenv;
+
+import javax.annotation.PostConstruct;
+
+public class DotenvConfig {
+
+    private Dotenv dotenv;
+
+    @PostConstruct
+    public void init() {
+        dotenv = Dotenv.load();
+    }
+
+    public String getJwtSecret() {
+        return dotenv.get("JWT_SECRET");
+    }
 }
